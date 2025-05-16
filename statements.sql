906de28a71fd48469ae584ba53ced0d52986d67d
Roberto Blasco~ <roberza2202@gmail.com>
2025-05-16 12:30:42 +0200

    Terminado

--- statements.sql
+++ statements.sql
@@ -136,14 +136,63 @@ UPDATE usuarios SET id_categoria = 4 WHERE id_usuario IN (4, 8, 12, 16, 20);
 -- PASO 4
 -- Tu código aquí
 
+SELECT usuarios.id_usuario,
+usuarios.nombre,
+usuarios. apellido,
+usuarios.email,
+usuarios.edad,
+roles.nombre_rol,
+categorias.nombre_categoria
+FROM usuarios
+JOIN roles ON usuarios.id_rol = roles.id_rol
+JOIN categorias ON usuarios.id_categoria = categorias.id_categoria;
+
 /* Relación tipo N:M */
 -- PASO 1
 -- Tu código aquí
 
+CREATE TABLE usuarios_categorias (
+	id_usuario_categoria INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT,
+    id_categoria INT
+);
+ALTER TABLE usuarios_categorias ADD FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario);
+ALTER TABLE usuarios_categorias ADD FOREIGN KEY (id_categoria) REFERENCES categorias(id_categoria);
 
 -- PASO 2
 -- Tu código aquí
 
+INSERT INTO usuarios_categorias (id_usuario, id_categoria) VALUES
+(1, 1), (1, 2), (1, 3),
+(2, 4), (2, 5),
+(3, 6), (3, 7),
+(4, 8), (4, 9), (4, 10);
 
 -- PASO 3
 -- Tu código aquí
+
+SELECT usuarios.id_usuario,
+usuarios.nombre,
+usuarios.apellido,
+usuarios.email,
+usuarios.edad,
+roles.nombre_rol,
+categorias.nombre_categoria
+FROM usuarios
+JOIN roles ON usuarios.id_rol = roles.id_rol
+JOIN categorias ON usuarios.id_categoria = categorias.id_categoria
+JOIN usuarios_categorias ON usuarios.id_usuario = usuarios_categorias.id_usuario;
+
+-- Con abreviaturas para cada tabla:
+
+SELECT 
+    u.id_usuario, u.nombre, u.apellido, u.email, u.edad,
+    r.nombre_rol, c.nombre_categoria
+FROM 
+    usuarios u
+JOIN 
+    roles r ON u.id_rol = r.id_rol
+JOIN 
+    usuarios_categorias uc ON u.id_usuario = uc.id_usuario
+JOIN 
+    categorias c ON uc.id_categoria = c.id_categoria;
